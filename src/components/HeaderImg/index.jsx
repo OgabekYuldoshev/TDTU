8444e2d0612917f0674fe7dfbd3747d24177a0f4
OgabekYuldoshev <yuldashoff1@gmail.com>
2022-06-24 15:25:51 +0500

    fix

--- src/components/HeaderImg/index.jsx
+++ src/components/HeaderImg/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-const HeaderImg = ({text}) => {
+const HeaderImg = ({ text }) => {
 
-    const {t} = useTranslation()
+    const { t } = useTranslation()
 
     return (
         <div className=''>
@@ -14,14 +14,14 @@ const HeaderImg = ({text}) => {
                     backgroundRepeat: 'no-repeat',
                     backgroundPosition: 'center'
                 }}
-                className={"bg-contain bg-center bg-no-repeat"}>
+                className={"bg-contain bg-center bg-no-repeat md:h-[400px] h-[200px] min-h-[200px] flex items-center justify-center"}>
                 <div
-                    className="py-[300px] sm:grid-cols-2 grid-cols-1 gap-10 flex flex-col justify-center items-center text-center">
-                    <span className={"font-extrabold text-5xl text-white uppercase"}>{t(`${text}`)}</span>
-                    <svg className={"w-full"} height="33" viewBox="0 0 1137 33" fill="none"
-                         xmlns="http://www.w3.org/2000/svg">
+                    className="sm:grid-cols-2 grid-cols-1 gap-5 flex flex-col justify-center items-center text-center">
+                    <span className={"font-extrabold md:text-3xl sm:tex-xl text-lg text-white uppercase"}>{t(`${text}`)}</span>
+                    <svg className='m-auto mx-12 px-12 w-full h-4' viewBox="0 0 1137 33" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path d="M2 2.5H527L567.75 30.5L608.5 2.5H1134.5" stroke="white" stroke-width="4"
-                              stroke-linecap="round"/>
+                            stroke-linecap="round" />
                     </svg>
                 </div>
             </div>
